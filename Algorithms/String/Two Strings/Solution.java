c5b78ef54b248e82576e92e08af76ae061a5ca5c
mukleshkumar <76259188+muklesh@users.noreply.github.com>
2020-12-20 14:14:35 +0530

    Create Solution.java

---
+++ Algorithms/String/Two Strings/Solution.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        HashSet<Character> hs=new HashSet<Character>();
+        HashSet<Character> hs1=new HashSet<Character>();
+        for(int i=0;i<s1.length();i++)
+        {
+            hs.add(s1.charAt(i));
+        }
+        for(int i=0;i<s2.length();i++)
+        {
+            hs1.add(s2.charAt(i));
+        }
+        hs.retainAll(hs1);
+
+        if(!hs.isEmpty())
+        {
+            return "YES";
+        }
+        else{
+            return "NO";
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
